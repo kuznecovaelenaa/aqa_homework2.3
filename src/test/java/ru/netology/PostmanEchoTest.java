21d37f1206c41069022d43f6ba6748ba7bb4f110
Elena <kuznecovaelenaa@yandex.ru>
2022-07-29 20:42:52 +0300

    first commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void firstTest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+           .baseUri("https://postman-echo.com")
+           .contentType("text/plain; charset=UTF-8")
+           .body("some data")
+        .when()
+           .post("/post")
+        .then()
+           .statusCode(200)
+//         .body("data", equalTo("some value"))
+//         .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
